9ade1b22ed922155c067bce130abab0b715b2f18
Nicolletr <78945498+Nicolletr@users.noreply.github.com>
2023-09-23 22:03:12 -0500

    Add Files

---
+++ src/permisos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de restaurante</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
+</head>
+<body>
+    
+
+<!-- Main content -->
+<section class="content">
+    <div class="error-page">
+        <h2 class="headline text-danger"> 404</h2>
+
+        <div class="error-content">
+            <h3><i class="fas fa-exclamation-triangle text-danger"></i> Oops! Área Restringida.</h3>
+
+            <p>
+                No tienes permisos.
+
+            </p>
+            <a href="dashboard.php">Regresar al Sistema</a>.
+        </div>
+        <!-- /.error-content -->
+    </div>
+    <!-- /.error-page -->
+</section>
+<!-- /.content -->
+
+</body>
+</html>
